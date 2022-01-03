bc2f7bd4a1f52ba2835bab5e735a867ee467b4ea
qlinhta <qlinh.ta@gmail.cpm>
2022-01-03 15:39:24 +0100

    3 JAN 2022

--- src/graph.cpp
+++ src/graph.cpp
@@ -4,9 +4,14 @@
 
 #include "graph.h"
 #include "bits/stdc++.h"
+using namespace std;
 
 Graph::Graph() {
-    std::string line;
-
+    string read;
+    getline(cin, line);
+    read = read.substr(6);
+    int blank = read.find(' ');
+    vertices = stoi(read.substr(0, blank));
+    edges = stoi(read.substr(blank));
 
 }
