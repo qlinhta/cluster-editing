d627f90840cb775795d4a9e27defb47c369589b4
qlinhta <linhtq.sfu@gmail.com>
2022-02-03 11:07:17 +0100

    Update 3 Feb 2022

---
+++ source/noSignals/fileHandler.py
@@ -0,0 +1,24 @@
+import sys
+
+
+class File:
+    def read_file(self):
+        try:
+            data = [line.strip() for line in sys.stdin]
+        except FileNotFoundError:
+            raise Exception("File not found Please add correct file path")
+
+        number_of_vertices = int(data[0].split()[2])
+        edges = []
+
+        for d in data[1:]:
+            u, v = [int(vertex) for vertex in d.split()]
+            edges.append((min(u - 1, v - 1), max(u - 1, v - 1)))
+
+        return number_of_vertices, edges
+
+    def write_results(self, solution):
+        f = open('Output.txt', 'w')
+        for sol in solution:
+            f.write(sol + '\n')
+        f.close()
---
+++ source/withSignals/main.py
@@ -0,0 +1,31 @@
+import time
+import sys
+
+from SIGTERM import Killer
+from kernelizedMultiCCInstance import KernelizedMultiCCInstance
+from fileHandler import File
+
+if __name__ == '__main__':
+    opt = [i for i in range(5, 55)]
+    weight = 10  # you can default from here
+
+    killer = Killer()
+
+    start_time = time.time()
+    # start processing
+    f = File()
+    number, edges = f.read_file()
+    instance = KernelizedMultiCCInstance(number, edges)
+
+    # running the greedy BFS solution
+    instance.greedy_bfs_fill(opt, weight, killer)
+    sol = instance.get_sol()
+
+    # output to console
+    for s in sol:
+        sys.stdout.write(s + "\n")
+
+    # writing a solution in a file
+    f.write_results(sol)
+
+    print("--- Total Time %s seconds ---" % (time.time() - start_time))
